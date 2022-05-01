2ff279b2bf09f0ba4fcfcee05ba43ca045120503
VinaySingla1235 <102358007+VinaySingla1235@users.noreply.github.com>
2022-05-01 12:38:22 +0530

    Revert "Added different modes"
    This reverts commit 2478175b929ab921bb606851492d2e6fb2884a89.

--- src/App.js
+++ src/App.js
@@ -25,20 +25,8 @@ function App() {
       setAlert(null);
     },1500);
   }
-  const removeBodyClasses=()=>{
-    document.body.classList.remove('bg-dark');
-    document.body.classList.remove('bg-light');
-    document.body.classList.remove('bg-warning');
-    document.body.classList.remove('bg-danger');
-    document.body.classList.remove('bg-success');
-   // document.body.classList.remove('bg-dark');
-   // document.body.classList.remove('bg-dark');
-  }
   //let modeRev="dark";
-  const toggleMode=(cls)=>{
-    console.log(cls);
-    removeBodyClasses();
-    document.body.classList.add('bg-'+cls);
+  const toggleMode=()=>{
     if(Mode ==="light"){
       setMode("dark");
       revMode("light");
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -40,19 +40,17 @@ export default function Navbar(props) {
               </Link>
             </li>
           </ul>
-          <div className="d-flex">
-            <div className="bg-dark rounded mx-2" onClick={()=>{props.toggleMode('dark')}}style={{height:'30px',width:'30px', cursor:'pointer'}}></div>
-            <div className="bg-danger rounded mx-2" onClick={()=>{props.toggleMode('danger')}}style={{height:'30px',width:'30px', cursor:'pointer'}}></div>
-            <div className="bg-success rounded mx-2" onClick={()=>{props.toggleMode('success')}}style={{height:'30px',width:'30px', cursor:'pointer'}}></div>
-            <div className="bg-warning rounded mx-2" onClick={()=>{props.toggleMode('warning')}}style={{height:'30px',width:'30px', cursor:'pointer'}}></div>
-          </div>
+          {/*<form className="d-flex">
+          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+          <button className="btn btn-outline-primary" type="submit">Search</button>
+          </form>*/}
           <div className="form-check form-switch normal-dark mx-2">
             <input
               className="form-check-input"
               type="checkbox"
               role="switch"
               id="normal-dark"
-              onClick={()=>{props.toggleMode(null)}}
+              onClick={props.toggleMode}
             />
             <label className={`form-check-label text-${props.modeRever}`} htmlFor="normal-dark">
               Enable {props.modeRever} Mode
